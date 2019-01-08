155855df0c208580d940870bf422e4ffe468e085
JoanChirinos <jchirinos@stuy.edu>
2019-01-08 16:26:47 -0500

    l0l

--- helpers.c
+++ helpers.c
@@ -35,9 +35,13 @@ struct deck* get_white_deck() {
   // yeeting lots and lots of memory
   free(raw_cards);
 
+  printf("cards b4: %s\n", "original");
+  print_cards(cards);
+  printf("\n\n\n\n\n");
   // setting struct values
   white->cards = cards;
-  // print_cards(white->cards);
+
+  print_cards(white->cards);
   white->size = size(cards);
   white->card_at = 0;
 
